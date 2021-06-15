fd724b9e400ee48a216b29551f251dc4469d7bcc
Nolan Kuza <nkuza8@gmail.com>
2021-06-15 10:55:08 -0400

    [sim] Disable configuration

--- Simulation/src/main/java/com/dekaresearch/simulation/SimulationConstants.java
+++ Simulation/src/main/java/com/dekaresearch/simulation/SimulationConstants.java
@@ -6,4 +6,5 @@ public class SimulationConstants {
      * Change this to enable or disable all simulation related code
      */
     public static boolean isSimulation = true;
+    public static boolean isRomi = true;
 }
